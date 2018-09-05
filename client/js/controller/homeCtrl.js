4fb26824ade4e7d422b967b737d42794df638927
Geison Machado <geisonmcd@gmail.com>
2018-09-04 22:54:16 -0300

    Botao de corre adicionado

--- client/js/controller/homeCtrl.js
+++ client/js/controller/homeCtrl.js
@@ -1,20 +1,38 @@
 angular.module("customerApp").controller("homeCtrl", ["$scope", "$location", function ($scope, $location){
     
-    $scope.customers;
+    $scope.customers = [{name:'geison'},{name:'dilon'}, {name:'davi'}, {name:'dilei'}];
     $scope.newCustomer;
     $scope.index = 0;
     $scope.rodadas;
     
     $scope.add = function(newCustomer) {
+        if (!newCustomer) return;
         $scope.customers = $scope.customers || [];
-        $scope.customers.push(angular.copy({customerName: newCustomer}));
+        $scope.customers.push(angular.copy( {name: newCustomer} ));
         delete $scope.newCustomer;
-    }
-
+    };
 
     $scope.addRodada = function(){
         $scope.rodadas = $scope.rodadas || [];
-        $scope.rodadas.push($scope.rodadas.length+ 1);
-    }
+        var rodada = { id:$scope.index };
+        for (i=0; i<$scope.customers.length ; i++) {
+            rodada[$scope.customers[i].name] = rodada[$scope.customers[i].name] || {};
+            rodada[$scope.customers[i].name].points = 10;
+        }
+        $scope.rodadas.push(rodada);
+        $scope.index++;
+    };
+
+    $scope.corre = function (customer) {
+        if (!$scope.rodadas) return;
+        var rodadaJogador = $scope.rodadas[$scope.index-1][customer.name];
+        if (rodadaJogador.status === 'correu') {
+            rodadaJogador.points++;
+            rodadaJogador.status = 'jogou';
+        } else {
+            rodadaJogador.points--;
+            rodadaJogador.status = 'correu';
+        }
+    };
     
 }]);
